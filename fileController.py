439e8373b48dde6b41bffca71d28e1ab04f1913d
Victor Fernandes <90485435+VictorFMag@users.noreply.github.com>
2023-11-27 12:26:44 -0300

    Add files via upload

--- fileController.py
+++ fileController.py
@@ -1,6 +1,5 @@
+# Funções auxiliares
 import os
-
-caminho_pasta = 'file_parts'
 
 def contar_arquivos_em_pasta(caminho_pasta):
     if os.path.exists(caminho_pasta) and os.path.isdir(caminho_pasta):
@@ -24,7 +23,7 @@ def limpaPasta(caminho_pasta):
     
     # Loop para apagar cada arquivo na pasta
     for arquivo in arquivos:
-        caminho_completo = os.path.join("file_parts", arquivo)
+        caminho_completo = os.path.join(caminho_pasta, arquivo)
         
         # Verifica se é um arquivo (e não uma subpasta)
         if os.path.isfile(caminho_completo):
@@ -35,13 +34,9 @@ def limpaPasta(caminho_pasta):
 import heapsort as hs
 
 def divide_arquivo(nome_arquivo, linhas_por_arquivo):
-    if contar_arquivos_em_pasta(caminho_pasta) == 0:
+    if contar_arquivos_em_pasta("file_parts/minor_files") == 0:
         
         with open(nome_arquivo, 'r') as arquivo_origem:
-            if not os.path.exists("file_parts/bigger_files"):
-                os.makedirs("file_parts/bigger_files")
-                print("Pasta criada com sucesso!")
-            
             indice_arquivo = 0
             linha = arquivo_origem.readline()
             
@@ -49,6 +44,7 @@ def divide_arquivo(nome_arquivo, linhas_por_arquivo):
                 caminho_subpasta = os.path.join('file_parts', 'minor_files')
                 if not os.path.exists(caminho_subpasta):
                     os.makedirs(caminho_subpasta)
+                    print("Pasta minor_files criada com sucesso!")
                 nome_parte = os.path.join(caminho_subpasta, f'parte_{indice_arquivo}.txt') # Cria os arquivos dentro da pasta
                 
                 with open(nome_parte, 'w') as parte_arquivo:
@@ -67,28 +63,53 @@ def divide_arquivo(nome_arquivo, linhas_por_arquivo):
         print(f'{indice_arquivo} partes criadas.')
     
     else:
-        limpaPasta(caminho_pasta)
+        limpaPasta('file_parts/minor_files')
         divide_arquivo(nome_arquivo, linhas_por_arquivo)
 
 #=======================================================================================================================
 
-def merge_files(): # falta arrumar isso
-    caminho_subpasta = os.path.join('file_parts', 'bigger_files')
+def merge_files(arquivos_por_merge):
+    caminho_subpasta = os.path.join('file_parts', 'medium_files')
     if not os.path.exists(caminho_subpasta):
         os.makedirs(caminho_subpasta)
+        print("Pasta medium_files criada com sucesso!")
+    else:
+        limpaPasta(caminho_subpasta)
 
-    arquivos = os.listdir(caminho_pasta)
-    partes_ordenadas = [sorted(arquivos[i:i+5]) for i in range(0, len(arquivos), 5)]
+    #
 
-    for i, partes in enumerate(partes_ordenadas):
-        with open(os.path.join(caminho_subpasta, f"arquivo_{i}_ordenado.txt"), "w") as arquivo_ordenado:
-            for parte in partes:
-                with open(os.path.join(caminho_pasta, parte), "r") as arquivo_parte:
-                    arquivo_ordenado.write(arquivo_parte.read())
+    for i in range(0, contar_arquivos_em_pasta('file_parts/minor_parts')+1, 5):
+        arrOfLines = []
+        arrOfArquives = []
+        for j in range(arquivos_por_merge):
+            with open(f'file_parts/minor_files/parte_{j}.txt', 'r') as file:
+                linhas = file.readlines()
+                arrOfLines.append(linhas[0])
+                novoArquivo = Arquivo(0, j)
+                arrOfArquives.append(novoArquivo)
 
-def achaMenor(*nums):
-    menor = nums[0]
-    for num in nums[1:]:
-        if num < menor:
-            menor = num
-    return menor
+        cont = 0
+        nome_arquivo_medio = os.path.join(caminho_subpasta, f'parte_media_{cont}.txt') # Cria os arquivos dentro da pasta
+        with open(nome_arquivo_medio, 'w') as arquivo_medio:
+            for j in range(len(arrOfLines)):
+                menor = min(arrOfLines)
+                arquivo_medio.write(menor)
+                idxArquivoComMenor = arrOfLines.index(menor)
+                
+                linha = Arquivo.avancarPointer(Arquivo.encontrar_por_id(arrOfArquives, idxArquivoComMenor))
+                #arrOfLines[idxArquivoComMenor] = 
+        cont+=1
+
+class Arquivo:
+    def __init__(self, arquivo_id, ultima_linha_analisada):
+        self.id = arquivo_id
+        self.ultimaLinhaAnalisada = ultima_linha_analisada
+    
+    def encontrar_por_id(arrOfArquives, arquivo_id):
+        for arquivo in arrOfArquives:
+            if arquivo.id == arquivo_id:
+                return arquivo
+        return None  # Retorna None se não encontrar o arquivo com o ID especificado
+    
+    def avancarPointer(arquivo_id):
+        arquivo_id.ultimaLinhaAnalisada += 1
--- main.py
+++ main.py
@@ -2,5 +2,6 @@ import fileController as FC
 
 nome_arquivo = 'ordExt_teste.txt'
 linhas_por_arquivo = 100
+arquivos_por_merge = 10
 FC.divide_arquivo(nome_arquivo, linhas_por_arquivo)
-FC.merge_files()
+FC.merge_files(arquivos_por_merge)
